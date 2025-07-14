73eee122cb056cbe33d1ac66e636adfd6ee2c79e
Khoa Nguyen <nguyendangkhoa211@gmail.com>
2025-07-14 11:56:03 +0800

    create first tool

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
-def main():
-    print("Hello from react-agent-from-scratch!")
+from dotenv import load_dotenv
+from langchain.agents import tool
+
+
+load_dotenv()
+
+# create first tool
+# IMPORTANT: description helps LLM decide whether to use this tool or not
+@tool
+def get_text_length(text: str) -> int:
+    """Returns the length of a text by characters"""
+    print(f"get_text_length enter with {text=}")
+    text = text.strip("'\n").strip('"') # strip away non-alphabetic characters
+    return len(text)
+
+
 
 
 if __name__ == "__main__":
-    main()
+    print("Hello ReAct LangChain!")
+    print(get_text_length.invoke(input={"text": "LangChain Expression Language"}))
